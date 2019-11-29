445090d1dce2db14faab3279d0eee7d9fb5f5018
Serhey Popovych <serhe.popovych@gmail.com>
2019-11-29 15:15:03 +0000

    simple-cdd-ext/debian: Preseed mirrors preferring kernel cmdline values
    To support network installations from non default mirrors (e.g. ISO
    shared via http server) we commented out mirror/http/hostname and
    mirror/http/directory values in network preseed file.
    
    While this serves good if we specify value at kernel command line
    explicitly it does not cover cases when old and archived distrubution
    has it's hostname (and possibly path) changed (e.g. from deb.debian.org
    to archive.debian.org) requiring user to always supply mirror/http/*
    variables on kernel command line even if he/she does not want to use
    non default mirrors.
    
    Use new API to read profiles configuration files to find suitable
    defaults for mirror/http/* preseeds in case of they aren't given
    on kernel command line. While there handle apt-setup/security_host
    in same manner.
    
    Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

--- debian/early_command.sh
+++ debian/early_command.sh
@@ -1,12 +1,62 @@
 #!/bin/sh
 
-# Requires: debconf-get, debconf-set
+# Requires: rm, debconf-set-selections
 
 ################################################################################
 
 ## Source common helpers
 
 . '/cdrom/simple-cdd/common/bootstrap.sh'
+
+## Preseed mirrors based either on command line options or on profile configs
+
+# Usage: read_profiles_conf_cb__mirrors ...
+read_profiles_conf_cb__mirrors()
+{
+    local method host dir
+
+    eval "$(split_url "$debian_mirror")"
+    echo "mirror_proto='$method' mirror_host='$host' mirror_dir='$dir'"
+
+    eval "$(split_url "$security_mirror")"
+    echo "sec_host='$host' sec_dir='$dir'"
+}
+
+read_profiles_conf 'read_profiles_conf_cb__mirrors'
+
+# Usage: set_selections_add <file> <opt> <dflt_val>
+set_selections_add()
+{
+    local func="${FUNCNAME:-set_selections_add}"
+
+    local file="${1:?missing 1st arg to ${func}() <file>}"
+    local opt="${2:?missing 2d arg to ${func}() <opt>}"
+    local val="${3:?missing 3rd arg to ${func}() <val>}"
+
+    val="$(get_cmdline_var "$opt" "$val")"
+
+    echo "d-i $opt string $val" >>"$file"
+}
+
+f='/tmp/.simple-cdd-early_command.sh-preseed'
+
+# Set country explicitly here for manual to make sure choose-mirror
+# does not start selecting mirrors based on country as we provide
+# explicit one either via commandline (prefferred) or via simple-cdd
+# profile configuration files.
+set_selections_add "$f" 'mirror/country'          'manual'
+
+set_selections_add "$f" 'mirror/protocol'         "${mirror_proto}"
+set_selections_add "$f" 'mirror/http/hostname'    "${mirror_host}"
+set_selections_add "$f" 'mirror/http/directory'   "${mirror_dir}"
+set_selections_add "$f" 'apt-setup/security_host' "${sec_host}${sec_dir%/}"
+
+# We cannot use debconf-set here since apt-setup/security_host
+# isn't loaded at early command execution as template provided
+# in apt-setup-udeb which neither loaded nor present in initrd.
+debconf-set-selections "$f"
+
+rm -f "$f" ||:
 
 # Executed from preseed/early_command
 exit 0
