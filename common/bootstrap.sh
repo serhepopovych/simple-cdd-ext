f9ce289249b9378ae51e1ee556517f4e6b7a8b24
Serhey Popovych <serhe.popovych@gmail.com>
2019-12-04 20:05:00 +0000

    simple-cdd-ext: Add helper to create shell wrapper on any executable
    It might be necessary to catch specific invocation (i.e. with specific
    arguments) of binary in installer environment to perform specific
    actions before (pre hook) and after (post hook) execution.
    
    For example in debian-installer we might want to catch udpkg calls to
    patch postinst scripts after --unpack operations. To do this post
    hook will be used that checks for unpack option with specific package
    to consider if postinst script patch should be applied.
    
    Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

--- common/bootstrap.sh
+++ common/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Requires: tr, sed, sort
+# Requires: mkdir, rmdir, rm, chmod, mv, cat, tr, sed, sort
 
 [ -z "${__bootstrap_sh__}" ] || return 0
 __bootstrap_sh__=1
@@ -178,6 +178,228 @@ valid_domain()
     [ -n "${name##[.-]*}" -a -n "${name%%*[-.]}" ] || return
 
     echo "$name" | grep -q '^[A-Za-z0-9-]\+$'
+}
+
+# Usage: rights_human2octal <rights>
+rights_human2octal()
+{
+    local func="${FUNCNAME:-rights_human2octal}"
+
+    # rwxr-xr-x (755), rwsrwSrwT (7766)
+    local rights="${1:?missing 1st arg to ${func}() <rights>}"
+    [ ${#rights} -eq 9 ] || return
+
+    local val=0
+    local g v s c C r
+
+    # groups: 3  2  1  0
+    # bits:  sgtrwxrwxrwx
+    for g in 2 1 0; do
+        v=0
+        s=0
+
+        if [ $g -ge 1 ]; then
+            c='s' && C='S'
+        else
+            c='t' && C='T'
+        fi
+
+        r="${rights#[r-][w-][xsStT-]}"
+        r="${rights%$r}"
+
+        # [r-]
+        case "$r" in
+            r??)  v=$((4 + v)) ;;
+            -??)  ;;
+            *)    return 1 ;;
+        esac
+
+        # [w-]
+        case "$r" in
+            ?w?)  v=$((2 + v)) ;;
+            ?-?)  ;;
+            *)    return 1 ;;
+        esac
+
+        # [xsStT-]
+        case "$r" in
+            ??x)  v=$((1 + v)) ;;
+            ??$c) v=$((1 + v)) && s=$((1 << g)) ;;
+            ??$C) s=$((1 << g)) ;;
+            ??-)  ;;
+            *)    return 1 ;;
+        esac
+
+        val=$((val | v << (3 * g) | s << (3 * 3)))
+
+        rights="${rights#$r}"
+    done
+
+    printf '%04o\n' "$val"
+}
+
+# Usage: file_rights_human <file>
+file_rights_human()
+{
+    local func="${FUNCNAME:-file_rights_human}"
+
+    local file="${1:?missing 1st arg to ${func}() <file>}"
+
+    [ -e "$file" ] || return
+
+    set -- $(ls -l "$file") || return
+
+    local rights="$1"
+    rights="${rights#?}"
+    [ ${#rights} -eq 9 ] || rights="${rights%?}"
+
+    case "$rights" in
+       [r-][w-][xsS-][r-][w-][xsS-][r-][w-][xtT-]) ;;
+       *) return 1 ;;
+    esac
+
+    echo "$rights"
+}
+
+# Usage: file_rights_octal <file>
+file_rights_octal()
+{
+    local func="${FUNCNAME:-file_rights_octal}"
+
+    local file="${1:?missing 1st arg to ${func}() <file>}"
+
+    local rights
+
+    rights="$(file_rights_human "$file")" || return
+    rights_human2octal "$rights"
+}
+
+# Usage: make_wrapper <bin> [<pre_func>] [<post_func>] [<keep>]
+make_wrapper()
+{
+    local func="${FUNCNAME:-make_wrapper}"
+
+    local bin="${1:?missing 1st arg to ${func}() <bin>}"
+    bin="$(command -v "$bin")" && [ -z "${bin##/*}" ] || return
+
+    local exe="${bin}.exe"
+    local rights
+
+    # Get permissions
+    rights="$(file_rights_octal "$bin")" || return
+
+    # Original binary
+    mv -f "$bin" "$exe" || return
+
+    # Create wrapper file
+    : >"$bin" || return
+    chmod "$rights" "$bin" || return
+
+    # Create hooks, if any
+    local hooks_dir="/tmp/${bin##*/}-hooks"
+    local keep="$hooks_dir/.keep"
+
+    mkdir -p "$hooks_dir" || return
+
+    if [ -n "$2" ]; then
+        local pre="$hooks_dir/pre"
+        "$2" >"$pre"  && [ -s "$pre" ] && chmod 0755 "$pre" || rm -f "$pre"
+    fi
+    if [ -n "$3" ]; then
+        local post="$hooks_dir/post"
+        "$3" >"$post" && [ -s "$post" ] && chmod 0755 "$post" || rm -f "$post"
+    fi
+
+    if ! rmdir "$hooks_dir" 2>/dev/null; then
+        [ -z "$4" ] || echo "$4" >"$keep" || return
+    fi
+
+    # Header
+    cat >>"$bin" <<_EOF
+#!/bin/sh
+
+bin='$bin'
+exe='$exe'
+
+hooks_dir='$hooks_dir'
+keep='$keep'
+pre=''
+post=''
+
+_EOF
+
+    # Body
+    cat >>"$bin" <<'_EOF'
+set -e
+
+exit_handler()
+{
+    local rc=$?
+
+    set +e
+
+    if [ -e "$keep" ]; then
+        :
+    else
+        [ -z "$pre" ] || rm -f "$pre" ||:
+        [ -z "$post" ] || rm -f "$post" ||:
+        rmdir "$hooks_dir" 2>/dev/null ||:
+    fi
+
+    return $rc
+}
+trap exit_handler EXIT
+
+# Usage: run_hook <hook> ...
+run_hook()
+{
+    local hook="$1"
+    shift
+
+    [ -x "$hook" ] || return 0
+
+    local HOOKS_DIR PRE POST KEEP THIS
+
+    HOOKS_DIR="$hooks_dir" \
+    PRE="$pre" \
+    POST="$post" \
+    KEEP="$keep" \
+    THIS="$hook" \
+        "$hook" "$@"
+}
+
+#### Execute real binary
+
+pre="$hooks_dir/pre" && run_hook "$pre" "$exe" "$@" || pre=''
+
+"$exe" "$@"
+
+post="$hooks_dir/post" && run_hook "$post" "$exe" "$@" || post=''
+_EOF
+}
+
+# Usage: echo_function_keep ...
+echo_function_keep()
+{
+    # This will be a part of pre/post scripts
+    cat <<'_EOF'
+# Usage: keep ...
+keep()
+{
+    local refcnt rc
+
+    if ! read -r refcnt <"$KEEP" >/dev/null 2>&1 ||
+       { refcnt=$((refcnt - 1)) && [ $refcnt -le 0 ]; }; then
+        rm -f "$KEEP" ||:
+        rc=0 # remove hooks
+    else
+        echo $refcnt >"$KEEP" ||:
+        rc=1 # keep hooks
+    fi
+
+    return $rc
+}
+_EOF
 }
 
 ## Install default exit handler
