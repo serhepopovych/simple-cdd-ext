serhepopovych/simple-cdd-ext

commit 445090d1dce2db14faab3279d0eee7d9fb5f5018

simple-cdd-ext/debian: Preseed mirrors preferring kernel cmdline values
To support network installations from non default mirrors (e.g. ISO
shared via http server) we commented out mirror/http/hostname and
mirror/http/directory values in network preseed file.

While this serves good if we specify value at kernel command line
explicitly it does not cover cases when old and archived distrubution
has it's hostname (and possibly path) changed (e.g. from deb.debian.org
to archive.debian.org) requiring user to always supply mirror/http/*
variables on kernel command line even if he/she does not want to use
non default mirrors.

Use new API to read profiles configuration files to find suitable
defaults for mirror/http/* preseeds in case of they aren't given
on kernel command line. While there handle apt-setup/security_host
in same manner.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

debian/early_command.sh
@@ -1,12 +1,62 @@
 #!/bin/sh
 
-# Requires: debconf-get, debconf-set
+# Requires: rm, debconf-set-selections
 
 ################################################################################
 
 ## Source common helpers
 
 . '/cdrom/simple-cdd/common/bootstrap.sh'
+
+## Preseed mirrors based either on command line options or on profile configs
+
+# Usage: read_profiles_conf_cb__mirrors ...
+read_profiles_conf_cb__mirrors()
+{
+    local method host dir
+
+    eval "$(split_url "$debian_mirror")"
+    echo "mirror_proto='$method' mirror_host='$host' mirror_dir='$dir'"
+
+    eval "$(split_url "$security_mirror")"
+    echo "sec_host='$host' sec_dir='$dir'"
+}
+
+read_profiles_conf 'read_profiles_conf_cb__mirrors'
+
+# Usage: set_selections_add <file> <opt> <dflt_val>
+set_selections_add()
+{
+    local func="${FUNCNAME:-set_selections_add}"
+
+    local file="${1:?missing 1st arg to ${func}() <file>}"
+    local opt="${2:?missing 2d arg to ${func}() <opt>}"
+    local val="${3:?missing 3rd arg to ${func}() <val>}"
+
+    val="$(get_cmdline_var "$opt" "$val")"
+
+    echo "d-i $opt string $val" >>"$file"
+}
+
+f='/tmp/.simple-cdd-early_command.sh-preseed'
+
+# Set country explicitly here for manual to make sure choose-mirror
+# does not start selecting mirrors based on country as we provide
+# explicit one either via commandline (prefferred) or via simple-cdd
+# profile configuration files.
+set_selections_add "$f" 'mirror/country'          'manual'
+
+set_selections_add "$f" 'mirror/protocol'         "${mirror_proto}"
+set_selections_add "$f" 'mirror/http/hostname'    "${mirror_host}"
+set_selections_add "$f" 'mirror/http/directory'   "${mirror_dir}"
+set_selections_add "$f" 'apt-setup/security_host' "${sec_host}${sec_dir%/}"
+
+# We cannot use debconf-set here since apt-setup/security_host
+# isn't loaded at early command execution as template provided
+# in apt-setup-udeb which neither loaded nor present in initrd.
+debconf-set-selections "$f"
+
+rm -f "$f" ||:
 
 # Executed from preseed/early_command
 exit 0

commit c7948df77a139a7bb16e145a3c8f6b429df2a041

simple-cdd-ext/debian: Normalize apt-setup/security_host URL in 91security
Since apt-setup commit d7019ea109b0 ("Use /debian-security path on
security.debian.org." 91security mirror generation helper changes
URL for security updates to http://${host}/debian-security from
http://${host} where ${host} is taken from apt-setup/security_host
preseed.

This introduces difference in how URL is handled in stretch+ releases
making apt-setup to produce incorrect URL with consequently appended
/debian-security parts caused by commit 445090d1dce2
("simple-cdd-ext/debian: Preseed mirrors preferring kernel cmdline
values") that preseeds apt-setup/security_host with full URL instead
of host part of the URL to workaround wheezy release movement to
archive.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

debian/early_command.sh
@@ -55,7 +55,7 @@ set_selections_add "$f" 'mirror/country'          'manual'
 set_selections_add "$f" 'mirror/protocol'         "${mirror_proto}"
 set_selections_add "$f" 'mirror/http/hostname'    "${mirror_host}"
 set_selections_add "$f" 'mirror/http/directory'   "${mirror_dir}"
-set_selections_add "$f" 'apt-setup/security_host' "${sec_host}${sec_dir%/}"
+set_selections_add "$f" 'apt-setup/security_host' "${sec_host}"
 
 ## Load generated preseed file
 

debian/partman/early_command.sh
@@ -67,6 +67,12 @@ for f in '50mirror' '91security' '92updates' '93backports'; do
         -e '/^		db_subst apt-setup\/service-failed HOST "\$host"$/,/^		fi$/ d'
 done
 
+## Patch /usr/lib/apt-setup/generators/91security to add /debian-security
+## to http://${apt-setup/security_host} URL when necessary (wheezy, jessie).
+
+sed -i '/usr/lib/apt-setup/generators/91security' \
+    -e 's,\(\(^\|\s\+\)echo\s\+"\S\+\)\(\s*http://\${host}\)/*\(\s\+\|"\),\1\3/debian-security\4,'
+
 ## Process *.excludes
 
 val="$(debconf-get 'base-installer/excludes')"