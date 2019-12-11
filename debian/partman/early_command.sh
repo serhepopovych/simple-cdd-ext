serhepopovych/simple-cdd-ext

commit c7948df77a139a7bb16e145a3c8f6b429df2a041

simple-cdd-ext/debian: Normalize apt-setup/security_host URL in 91security
Since apt-setup commit d7019ea109b0 ("Use /debian-security path on
security.debian.org." 91security mirror generation helper changes
URL for security updates to http://${host}/debian-security from
http://${host} where ${host} is taken from apt-setup/security_host
preseed.

This introduces difference in how URL is handled in stretch+ releases
making apt-setup to produce incorrect URL with consequently appended
/debian-security parts caused by commit 445090d1dce2
("simple-cdd-ext/debian: Preseed mirrors preferring kernel cmdline
values") that preseeds apt-setup/security_host with full URL instead
of host part of the URL to workaround wheezy release movement to
archive.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

debian/partman/early_command.sh
@@ -67,6 +67,12 @@ for f in '50mirror' '91security' '92updates' '93backports'; do
         -e '/^		db_subst apt-setup\/service-failed HOST "\$host"$/,/^		fi$/ d'
 done
 
+## Patch /usr/lib/apt-setup/generators/91security to add /debian-security
+## to http://${apt-setup/security_host} URL when necessary (wheezy, jessie).
+
+sed -i '/usr/lib/apt-setup/generators/91security' \
+    -e 's,\(\(^\|\s\+\)echo\s\+"\S\+\)\(\s*http://\${host}\)/*\(\s\+\|"\),\1\3/debian-security\4,'
+
 ## Process *.excludes
 
 val="$(debconf-get 'base-installer/excludes')"

debian/early_command.sh
@@ -55,7 +55,7 @@ set_selections_add "$f" 'mirror/country'          'manual'
 set_selections_add "$f" 'mirror/protocol'         "${mirror_proto}"
 set_selections_add "$f" 'mirror/http/hostname'    "${mirror_host}"
 set_selections_add "$f" 'mirror/http/directory'   "${mirror_dir}"
-set_selections_add "$f" 'apt-setup/security_host' "${sec_host}${sec_dir%/}"
+set_selections_add "$f" 'apt-setup/security_host' "${sec_host}"
 
 ## Load generated preseed file
 

commit c4821ebd1a8ff29476c4025921ab69aa9ba7eb13

simple-cdd-ext/debian: Fix incorrect ${host} vs $host in regex matching
When matching security_host variable reference in apt-setup 91security
generator correct string is $host, not ${host}. This was not tested
properly on Debian GNU/Linux 7 (wheezy) for which this patching intended
originally.

Fixes: c7948df77a13 ("simple-cdd-ext/debian: Normalize apt-setup/security_host URL in 91security")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

debian/partman/early_command.sh
@@ -79,7 +79,7 @@ done
 ## to http://${apt-setup/security_host} URL when necessary (wheezy, jessie).
 
 sed -i '/usr/lib/apt-setup/generators/91security' \
-    -e 's,\(\(^\|\s\+\)echo\s\+"\S\+\)\(\s*http://\${host}\)/*\(\s\+\|"\),\1\3/debian-security\4,'
+    -e 's,\(\(^\|\s\+\)echo\s\+"\S\+\)\(\s*http://\$host\)/*\(\s\+\|"\),\1\3/debian-security\4,'
 
 ## Process *.excludes
 