3f5d4bac1beb1e53d2ef43449746a6b587258a58
Serhey Popovych <serhe.popovych@gmail.com>
2022-02-10 11:52:42 +0200

    simple-cdd-ext/debian: Use upstream approach for btrfs subvolumes
    With commit c432c116e9ba ("Extend description of subvol support, close
    bug, update date stamp") and commit 500ee0c6f6ed ("Add minimal subvolume
    support for /.") in partman-btrfs repository support for installation
    to btrfs subvolume added to upstream with Debian GNU/Linux 11 (bullseye)
    release. This implementation prefferred over our during installation
    process leaving our subvol=root empty.
    
    While current and upstream implementation expectedly differs, both have
    same goal: use subvolume for root filesystem during installation.
    
    Replace our implementation in favour of upstream to keep compatibility
    and make sure releases prior to Debian GNU/Linux 11 (bullseye) support
    this upstream functionality.
    
    See more details about upstream implementation at
    https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=964818.
    
    Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

--- debian/partman/btrfs/source.sh
+++ debian/partman/btrfs/source.sh
@@ -5,8 +5,9 @@
 ################################################################################
 
 # Btrfs with mixed metadata and data block groups is used (mkfs.btrfs(8) -M
-# option) and compression enabled. System installed into "root" subvolume
-# to enable reuse of "/" filesystem as snapshots for LXC rootfs.
+# option) and compression enabled. System installed into "@rootfs" subvolume
+# to enable reuse of "/" filesystem with snapshots for LXC or systemd-nspawn(1)
+# rootfs.
 #
 # Note that Debian currently does not support installation to subvolumes
 # as well as configuring mixed medatada and data block groups mode: enable
@@ -16,31 +17,49 @@
 # boot partition created to save space as modern versions of GRUB 2.x can boot
 # from btrfs volume (tested at least with Debian GNU/Linux 7.x (wheezy)).
 
-## Add subvol=root as valid btrfs option
-file='/lib/partman/mountoptions/btrfs'
-if ! grep -q 'subvol=root' "$file"; then
-    echo 'subvol=root' >>"$file"
-fi
-
 ## Patch format code to use -M option if necessary and create 'root' subvolume
 
 recipe="${SIMPLE_CDD_PARTMAN_RECIPE_FILE##*/}"
 [ "$(to_bytes "$recipe")" -ge $((5 * GiB)) ] || opts='-M'
 
-sed -i '/lib/partman/commit.d/50format_btrfs' \
+# partman-btrfs
+#   c432c116e9ba Extend description of subvol support, close bug, update date stamp
+#   500ee0c6f6ed Add minimal subvolume support for /.
+sed -i '/lib/partman/fstab.d/btrfs' \
+    -e '/if \[ "\$mountpoint" = "\/tmp" \]; then/,$ b
+        /if \[ "\$mountpoint" = \/tmp \]; then/,/echo "\$path" "\$mountpoint" btrfs \$options/c\
+			if [ "$mountpoint" = "/tmp" ]; then\
+				rm -f $id/options/noexec\
+			fi\
+			if [ "$mountpoint" = "/" ]; then\
+				options="$(get_mountoptions $dev $id),subvol=@rootfs"\
+			else\
+				options=$(get_mountoptions $dev $id)\
+			fi\
+			# There is no btrfs fsck\
+			echo "$path" "$mountpoint" btrfs $options 0 0'
+
+sed -i '/lib/partman/mount.d/70btrfs' \
+    -e '/# import workaround from Kali'\''s partman-btrfs commit:7f43d2c/,$ b
+        /mount -t btrfs \${options:+-o "\$options"} \$fs \/target\$mp || exit 1/,/echo "umount \/target\$mp"/c\
+	# import workaround from Kali'\''s partman-btrfs commit:7f43d2c\
+	options="${options%,subvol=*}"\
+	#for removing the option subvol,when thats the only option\
+	#eg: options=="subvol=@", no comma present\
+	options="${options%subvol=*}"\
+	mount -t btrfs ${options:+-o "$options"} $fs /target"$mp" || exit 1\
+	if [ $mp = / ]; then\
+	    btrfs subvolume create /target$mp/@rootfs\
+	    chmod 755 /target$mp/@rootfs\
+	    umount /target$mp\
+	    options="${options:+$options,}subvol=@rootfs"\
+	    mount -t btrfs -o $options $fs /target$mp\
+	fi\
+	echo "umount /target$mp"'
+
+sed '/lib/partman/commit.d/50format_btrfs' \
     -e 'N
-        s,^\s\+\(mkfs\.btrfs \)\(-f \)\?\(.\+\) \\\s\+\$device \(.\+\)$,				\1\2'"${opts:+$opts }"'\3 \\\
-					 $device \4 \&\& \\\
-			log-output -t partman --pass-stdout \\\
-				mkdir -p /tmp/.simple-cdd-btrfs \&\& \\\
-			log-output -t partman --pass-stdout \\\
-				mount -t btrfs -o compress $device /tmp/.simple-cdd-btrfs \&\& \\\
-			log-output -t partman --pass-stdout \\\
-				btrfs subvolume create /tmp/.simple-cdd-btrfs/root \&\& \\\
-			log-output -t partman --pass-stdout \\\
-				umount /tmp/.simple-cdd-btrfs \&\& \\\
-			log-output -t partman --pass-stdout \\\
-				rmdir /tmp/.simple-cdd-btrfs || code=$?,;t
+	s,^\(\s\+mkfs\.btrfs \)\(-f \)\?\(.\+ \\\s\+\$device .\+\)$,\1\2'"${opts:+$opts }"'\3,;t
 	P
 	D'
 
